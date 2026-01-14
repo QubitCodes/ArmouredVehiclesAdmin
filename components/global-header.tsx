a14fb329577efea6018c4fdd448fbe84f9e8bd40
devnihad <nihad@wincept.in>
2026-01-14 17:03:55 +0530

    Remove unused CAPTCHA code and clean up account preferences form

--- components/global-header.tsx
+++ components/global-header.tsx
@@ -16,7 +16,7 @@ export function GlobalHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full bg-white">
-      <div className="container mx-auto flex h-16 items-center justify-between px-6">
+      <div className="container mx-auto flex h-16 items-center justify-between">
         {/* Logo */}
         <div className="flex items-center">
           <Image
